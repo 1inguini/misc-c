a0fd23a3a760e68124656b8c9f717bc90cfddf87
1inguini <the@1inguini.com>
2025-03-13 16:18:13 +0900

    errono、なんかキショいらしい

--- execd/execd.c
+++ execd/execd.c
@@ -2,10 +2,10 @@
 // execd by 1inguini since 2025-03-13
 
 #include <nolibc/nolibc.h>
+#define NOLIBC_IGNORE_ERRNO
 
 int main(int argc, char const *argv[])
 {
   char message[] = "Hello World!\n";
-  write(STDOUT_FILENO, message, strlen(message));
-  return 0;
+  sys_write(STDOUT_FILENO, message, strlen(message));
 }
