ccd14fe31bcd52d0cb2862bbc71417079ba5d537
1inguini <the@1inguini.com>
2025-03-13 18:12:38 +0900

    init self pipe

--- execd/execd.c
+++ execd/execd.c
@@ -4,22 +4,49 @@
 #include <nolibc/nolibc.h>
 #define NOLIBC_IGNORE_ERRNO
 
-typedef ssize_t syscall_result;
+typedef ssize_t syscall_result; // `sys_*`の返り値
+
 char usage[] = "execd: usage: execd $SUPERVISE_DIR $COMMAND $ARG1 $ARG2...\n";
+
+syscall_result execd_log(int fd, char const message[])
+{
+  return sys_write(fd, message, strlen(message));
+}
 
 // 前提が満たされてないことが確認されたときに早期リターンする用
 // ログを吐いたあとやユーザーにデバッグとかを求めるときは使わない
 void execd_bye()
 {
-  sys_write(STDERR_FILENO, usage, strlen(usage));
+  execd_log(STDERR_FILENO, usage);
   exit(EXIT_FAILURE);
 }
 
 int main(int argc, char const *argv[])
 {
+  // temporary variable to store result of previous syscall
+  syscall_result r = 0;
+
+  // 前提条件チェック
+  // BEGIN precondition-check
   if (argc < 2) // コマンド引数は2以上必要
   {
     execd_bye();
   }
   const char *sv_dir = argv[1];
+  // END precondition-check
+
+  // よくわからないがself pipeというものが必要らしい
+  // BEGIN self-pipe
+  int self_pipe[2];
+
+  // s6、というか skalibsがこのフラグを立ててたのでわからないが立てておく
+  // https://github.com/skarnet/skalibs/blob/main/src/libstddjb/selfpipe.c#L83
+  // daemontoolsもあとから`fcntl`で設定してた (`coe.h`,`ndelay.h`)
+  r = sys_pipe2(self_pipe, O_NONBLOCK | O_CLOEXEC);
+  if (r)
+  {
+    execd_log(STDERR_FILENO, "execd: unable to create self-pipe");
+    return EXIT_FAILURE;
+  }
+  // END self-pipe
 }
